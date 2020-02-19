55a48a4724316e7ca4100255740388ee1f974da2
Sergio Can <gersiopumas@gmail.com>
2020-02-19 10:08:58 -0500

    Styled Component SSR configuration

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import NextDocument from 'next/document';
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const styledComponentSheet = new StyledComponentSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => styledComponentSheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styledComponentSheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      styledComponentSheet.seal();
+    }
+  }
+}
