174699c486b88b63cf097509a63d81168ec37bc8
Sergio Can <gersiopumas@gmail.com>
2020-03-03 11:37:06 -0500

    Added footer Component

--- components/Template/index.js
+++ components/Template/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header';
 import SmartMenu from '../SmartMenu';
 import { Content } from './index.styled';
+import Footer from '../Footer';
 
 const Template = ({ children }) => (
   <div>
@@ -10,6 +11,7 @@ const Template = ({ children }) => (
       <SmartMenu />
       {children}
     </Content>
+    <Footer />
     <style global jsx>
       {`
         @import url('https://fonts.googleapis.com/css?family=Raleway:900&display=swap');
